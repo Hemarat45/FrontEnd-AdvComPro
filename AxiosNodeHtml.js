3622793ecc459678fb027748c4427a526371b58d
Hemarat45 <bungvas45@gmail.com>
2024-09-11 11:42:11 +0700

    g

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -3,8 +3,8 @@ const axios = require ('axios');
 const app = express();
 var bodyParser = require('body-parser');
 const path = require("path")
-
-const base_url = "http://node66897-env-5307524.proen.app.ruk-com.cloud:11449";
+const base_url = "http://http://localhost:3000";
+//const base_url = "http://node66897-env-5307524.proen.app.ruk-com.cloud:11449";
 
 app.set("views", path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
